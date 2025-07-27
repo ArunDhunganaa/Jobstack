423fab83ac41db26bc5850d31d8f7c5af13aef2f
Arun Dhungana <a.d.since03@gmail.com>
2025-07-27 06:58:20 +0545

    front page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gray-100 px-5 py-12 text-center text-gray-900 md:flex-row md:text-start lg:gap-12">
-      <div className="md:pr-30 max-w-prose">
+    <main className="bg-gray-100 text-gray-900 flex min-h-screen flex-col items-center justify-center gap-6 px-5 py-12 text-center md:flex-row md:text-start lg:gap-12">
+      <div className="max-w-prose md:pr-30">
         <Image
           src={logo}
           alt="Logo"
@@ -16,15 +16,16 @@ export default function Home() {
           className="mx-auto md:ms-0"
         />
         <h1 className="mb-24 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Create the{" "}
+          Find the{" "}
           <span className="t inline-block bg-gradient-to-r bg-clip-text text-[#2563EB]">
-            Perfect Resume
+            Perfect Job
           </span>{" "}
           in Minutes
         </h1>
-        <p className="mb-24 text-lg text-gray-500">
-          Our <span className="font-bold">AI resume builder</span> helps you
-          design a professional resume, even if you&apos;re not very smart.
+        <p className="text-gray-500 mb-24 text-lg">
+          Our <span className="font-bold">JobStack</span> helps you build your
+          resume, analyze your skills, and get AI-driven job recommendations —
+          all in one platform.
         </p>
         <Button asChild className="bg-[#2563EB] px-24 py-12 text-lg">
           <Link href="/home">Get started</Link>
